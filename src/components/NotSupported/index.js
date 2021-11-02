0eabc4ed46b8be8dbe6deb655fc16346e8dd340d
melnicenkovadik <melnicenkovadik@gmail.com>
2021-11-02 11:23:26 +0200

    host detected

--- src/components/NotSupported/index.js
+++ src/components/NotSupported/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
 
-const NotSupported = ({ onclick }) => (
-  <div className="not-supported">
-    Ambrosus is not supported on this network. Please&nbsp;
-    <p className="switch-text" role="presentation" onClick={() => onclick()}>
-      switch to Mainnet
-    </p>
-  </div>
-);
+const NotSupported = ({ onclick }) => {
+  const host = window.location.hostname;
+  const network = host === 'localhost' ? 'Testnet' : 'Mainnet';
+  console.log(network);
+  return (
+    <div className="not-supported">
+      Ambrosus is not supported on this network. Please&nbsp;
+      <p className="switch-text" role="presentation" onClick={() => onclick()}>
+        switch to {network}
+      </p>
+    </div>
+  );
+};
 NotSupported.propTypes = {
   onclick: PropTypes.func,
 };
