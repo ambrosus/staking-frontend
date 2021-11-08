9fe391262052fd5044affd82560e927284955993
melnicenkovadik <melnicenkovadik@gmail.com>
2021-11-08 10:12:39 +0200

    remove skeleton

--- src/components/Loader/LoadComponent.js
+++ src/components/Loader/LoadComponent.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactLoading from 'react-loading';
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
-import * as PropTypes from 'prop-types';
 
 const Loader = () => (
   <div className="loader">
@@ -9,34 +7,6 @@ const Loader = () => (
   </div>
 );
 
-const SkeletonString = ({ flag }) => {
-  const [show, setShow] = useState(false);
+const SkeletonString = () => <span className="skeleton" />;
 
-  useEffect(() => {
-    let timing;
-    if (flag) {
-      timing = setTimeout(() => {
-        setShow(flag);
-      }, 2000);
-    }
-    return () => show && clearTimeout(timing);
-  }, [show, flag]);
-
-  return (
-    <div className="skeleton">
-      <SkeletonTheme color="#FFFFFF" highlightColor="rgba(215, 215, 215, 0.53)">
-        <p>
-          <Skeleton
-            count={1}
-            duration={2}
-            style={{ border: '0px solid', outline: '0px solid' }}
-          />
-        </p>
-      </SkeletonTheme>
-    </div>
-  );
-};
-SkeletonString.propTypes = {
-  flag: PropTypes.bool,
-};
 export { Loader, SkeletonString };
