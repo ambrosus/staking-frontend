ad7e076fd1d986aba76ce1c439861ce717b51881
vitalii427 <vitalii427@yahoo.com>
2021-11-09 11:18:05 +0200

    Wrapper update

--- src/services/staking.wrapper.js
+++ src/services/staking.wrapper.js
@@ -11,7 +11,7 @@ const FIXEDPOINT = TEN.pow(18); // 1.0 ether
 const MINSHOWSTAKE = FIXEDPOINT.div(100); // 0.01 ether
 const THOUSAND = FIXEDPOINT.mul(1000); // 1000 ether
 
-const AVERAGING_PERIOD = 1 * 24 * 60 * 60; // 1 day
+const AVERAGING_PERIOD = 5 * 24 * 60 * 60; // 5 days
 
 const math = create(all, {
   number: 'BigNumber',
@@ -42,68 +42,56 @@ function formatFixed(bigNumber, digits = 18) {
 
 class StakingWrapper {
   constructor(providerOrSigner = null) {
-    //  console.log('StakingWrapper', providerOrSigner);
     if (!providerOrSigner) {
       providerOrSigner = new providers.JsonRpcProvider(
         process.env.REACT_APP_RPC_URL,
       );
     }
-    this.providerOrSigner = providerOrSigner;
+    this._providerOrSigner = providerOrSigner;
 
-    this.initPromise = this._initialize();
+    this._initPromise = this._initialize();
   }
 
   async _initialize() {
-    // const now = Math.floor(Date.now() / 1000);
-    // const blocksCount = Math.floor(AVERAGING_PERIOD / 5);
-    // const block = await this.providerOrSigner.provider.getBlock(-blocksCount);
-    // console.log(
-    //   'block',
-    //   blocksCount,
-    //   block.number,
-    //   now - block.timestamp - AVERAGING_PERIOD,
-    //   (now - block.timestamp) / blocksCount,
-    // );
-
     this.headContract = new ethers.Contract(
       headContractAddress,
       contractJsons.head.abi,
-      this.providerOrSigner,
+      this._providerOrSigner,
     );
     const contextContract = new ethers.Contract(
       await this.headContract.context(),
       contractJsons.context.abi,
-      this.providerOrSigner,
+      this._providerOrSigner,
     );
     const storageCatalogueContr = new ethers.Contract(
       await contextContract.storageCatalogue(),
       contractJsons.storageCatalogue.abi,
-      this.providerOrSigner,
+      this._providerOrSigner,
     );
     this.poolEventsEmitter = new ethers.Contract(
       await storageCatalogueContr.poolEventsEmitter(),
       contractJsons.poolEventsEmitter.abi,
-      this.providerOrSigner,
+      this._providerOrSigner,
     );
     this.poolsStore = new ethers.Contract(
       await storageCatalogueContr.poolsStore(),
       contractJsons.poolsStore.abi,
-      this.providerOrSigner,
-    );
-
-    const poolsCount = await this.poolsStore.getPoolsCount();
-    const poolsAddrs = await this.poolsStore.getPools(0, poolsCount);
-    this.pools = poolsAddrs.map(
-      (poolAddr) =>
-        new ethers.Contract(poolAddr, pool.abi, this.providerOrSigner),
+      this._providerOrSigner,
     );
   }
 
   async getPools() {
-    await this.initPromise;
+    await this._initPromise;
+
+    const poolsCount = await this.poolsStore.getPoolsCount();
+    const poolsAddrs = await this.poolsStore.getPools(0, poolsCount);
+    this._pools = poolsAddrs.map(
+      (poolAddr) =>
+        new ethers.Contract(poolAddr, pool.abi, this._providerOrSigner),
+    );
 
     return Promise.all(
-      this.pools.map(async (_pool, index) => {
+      this._pools.map(async (_pool, index) => {
         const info = await Promise.all([_pool.name(), _pool.active()]);
         return {
           index,
@@ -111,6 +99,7 @@ class StakingWrapper {
           address: _pool.address,
           abi: pool.abi,
           active: info[1],
+          contract:_pool,
         };
       }),
     );
@@ -120,15 +109,11 @@ class StakingWrapper {
     if (typeof index !== 'number') {
       throw new Error('no pool index provided');
     }
-    await this.initPromise;
+    await this._initPromise;
 
-    const poolContract = this.pools[index];
+    if (!this._pools) await this.getPools();
 
-    // console.log('getPoolData');
-    // console.log('pools:', await this.getPools());
-    // if (typeof index === 'number') console.log('apy:', await this.getAPY(index));
-
-    // console.log('count', await this.getPoolsCount());
+    const poolContract = this._pools[index];
 
     const [totalStakeInAMB, tokenPriceAMB, myStakeInTokens, poolDPY] =
       await Promise.all([
@@ -162,39 +147,27 @@ class StakingWrapper {
       poolAPY,
       estDR,
     };
-    // console.log(poolData);
+
     return poolData;
   }
 
   async _getDPY(index = null) {
-    await this.initPromise;
+    await this._initPromise;
+
+    const poolAddr = this._pools[index].address;
 
     const rewardEvents = await this.poolEventsEmitter.queryFilter(
       this.poolEventsEmitter.filters.PoolReward(null, null, null),
       -Math.floor(AVERAGING_PERIOD / 5),
     );
 
-    if (!rewardEvents || !rewardEvents.length) return 0;
+    if (!rewardEvents || rewardEvents.length < 2) return 0;
 
     const sortedPoolRewards = rewardEvents
-      .filter((event) => event.args.pool === this.pools[index].address)
+      .filter((event) => event.args.pool === poolAddr)
       .sort((a, b) => a.args.tokenPrice.gte(b.args.tokenPrice));
 
-    if (!sortedPoolRewards || !sortedPoolRewards.length) return 0;
-
-    // console.log(sortedPoolRewards);
-
-    // console.log(
-    //   'REWARD:',
-    //   await Promise.all(
-    //     sortedPoolRewards.map(async (event) => ({
-    //       n: event.blockNumber,
-    //       p: event.args.tokenPrice.toString(),
-    //       f: (await event.getTransactionReceipt()).from,
-    //       r: event.args.reward.toString(),
-    //     })),
-    //   ),
-    // );
+    if (!sortedPoolRewards || sortedPoolRewards.length < 2) return 0;
 
     const [firstReward, lastReward] = await Promise.all(
       sortedPoolRewards
@@ -208,8 +181,6 @@ class StakingWrapper {
         })),
     );
 
-    if (!firstReward || !lastReward) return 0;
-
     const dpy = exprDPY.evaluate({
       s1: math.bignumber(firstReward.tokenPrice.toString()),
       s2: math.bignumber(lastReward.tokenPrice.toString()),
@@ -217,7 +188,6 @@ class StakingWrapper {
       t2: firstReward.timestamp,
     });
 
-    // console.log('dpy:', dpy.toString());
     return dpy;
   }
 }
