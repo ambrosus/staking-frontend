b75d75eb51e8d705ba8c64e1fdba5f2ea79a02de
melnicenkovadik <melnicenkovadik@gmail.com>
2021-11-09 20:14:40 +0200

    useLogin fix

--- src/utils/useLogIn.js
+++ src/utils/useLogIn.js
@@ -12,79 +12,69 @@ const useLogIn = () => {
   const history = useHistory();
 
   const logIn = async () => {
-    if (ethereum) {
-      handleEthereum();
-    } else {
-      window.addEventListener('ethereum#initialized', handleEthereum, {
-        once: true,
-      });
-    }
+    if (ethereum && ethereum.isMetaMask) {
+      await ethereum
+        .request({
+          method: 'wallet_requestPermissions',
+          params: [
+            {
+              eth_accounts: {},
+            },
+          ],
+        })
+        .then(async (e) => {
+          if (e) {
+            history.push('/staking');
+            storageService.set('auth', true);
+            appStore.setAuth(true);
+            const provider = new providers.Web3Provider(ethereum);
+            provider.on('network', (newNetwork, oldNetwork) => {
+              if (oldNetwork) {
+                window.location.reload();
+              }
+            });
 
-    async function handleEthereum() {
-      if (ethereum && ethereum.isMetaMask) {
-        await ethereum
-          .request({
-            method: 'wallet_requestPermissions',
-            params: [
-              {
-                eth_accounts: {},
-              },
-            ],
-          })
-          .then(async (e) => {
-            if (e) {
-              history.push('/staking');
-              storageService.set('auth', true);
-              appStore.setAuth(true);
-              const provider = new providers.Web3Provider(ethereum);
-              provider.on('network', (newNetwork, oldNetwork) => {
-                if (oldNetwork) {
-                  window.location.reload();
+            provider
+              .listAccounts()
+              .then((accounts) => {
+                const defaultAccount = accounts[0];
+                if (defaultAccount) {
+                  appStore.setAuth(true);
+                } else {
+                  storageService.set('auth', false);
+                }
+              })
+              .catch((error) => {
+                if (error) {
+                  storageService.set('auth', false);
                 }
               });
-
-              provider
-                .listAccounts()
-                .then((accounts) => {
-                  const defaultAccount = accounts[0];
-                  if (defaultAccount) {
-                    appStore.setAuth(true);
-                  } else {
-                    storageService.set('auth', false);
-                  }
-                })
-                .catch((error) => {
-                  if (error) {
-                    storageService.set('auth', false);
-                  }
-                });
-            }
-          })
-          .catch((e) => {
-            if (e) {
-              storageService.set('auth', false);
-              appStore.setAuth(false);
-            }
-          });
-      } else {
-        alertStore.addNotification({
-          content: InstallMetamaskAlert,
-          container: 'bottom-right',
-          animationIn: ['animated', 'fadeIn'],
-          animationOut: ['animated', 'fadeOut'],
+          }
+        })
+        .catch((e) => {
+          if (e) {
+            storageService.set('auth', false);
+            appStore.setAuth(false);
+          }
         });
-      }
-      if (
-        navigator.userAgent.includes('iPhone') ||
-        navigator.userAgent.includes('Android')
-      ) {
-        alertStore.addNotification({
-          content: FromPhoneDeviseEnter,
-          container: 'bottom-right',
-          animationIn: ['animated', 'fadeIn'],
-          animationOut: ['animated', 'fadeOut'],
-        });
-      }
+    } else {
+      alertStore.addNotification({
+        content: InstallMetamaskAlert,
+        container: 'bottom-right',
+        animationIn: ['animated', 'fadeIn'],
+        animationOut: ['animated', 'fadeOut'],
+      });
+    }
+    if (
+      navigator.userAgent.includes('iPhone') ||
+      navigator.userAgent.includes('Android')
+    ) {
+      alertStore.addNotification({
+        content: FromPhoneDeviseEnter,
+        container: 'bottom-right',
+        animationIn: ['animated', 'fadeIn'],
+        animationOut: ['animated', 'fadeOut'],
+      });
     }
   };
   return { logIn };
