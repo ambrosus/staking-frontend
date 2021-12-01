5e5d20c7f58ee749cfb56eb2e45ce812061caca0
melnicenkovadik <melnicenkovadik@gmail.com>
2021-12-01 13:51:15 +0200

    gte() remove from useState

--- src/pages/Staking/components/InfoBlock/InfoBlock.js
+++ src/pages/Staking/components/InfoBlock/InfoBlock.js
@@ -6,7 +6,7 @@ import { BigNumber } from 'ethers';
 import { observer } from 'mobx-react-lite';
 
 import Paragraph from '../../../../components/Paragraph';
-import { FIXED_POINT, formatRounded } from '../../../../services/numbers';
+import { formatRounded } from '../../../../services/numbers';
 import earningsIcon from '../../../../assets/svg/last24h.svg';
 import pieChartOutlineIcon from '../../../../assets/svg/pie_chart_outline.svg';
 import errorOutlineIcon from '../../../../assets/svg/error_outline.svg';
@@ -52,18 +52,17 @@ const InfoBlock = observer(({ poolsArr, account }) => {
         (acc, curr) => acc.add(curr),
         BigNumber.from('0'),
       );
-      setTotalStaked(totalStakeSum.gte(FIXED_POINT) && totalStakeSum);
+      setTotalStaked(totalStakeSum);
       setTotalStakedInUsd(
         totalStakeSum &&
           appStore.tokenPrice &&
-          totalStakeSum.gte(FIXED_POINT) &&
           formatRounded(totalStakeSum) * appStore.tokenPrice,
       );
     }
   };
 
   const getInfo = async () => {
-    // todo: do refresh here
+    // todo: do refresh here ++
     if (poolsArr.length > 0) {
       poolsRewards = [];
       myTotalStake = [];
