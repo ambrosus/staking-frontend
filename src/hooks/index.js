09827a5cd00d916b2639fb72838db454812c4541
melnicenkovadik <melnicenkovadik@gmail.com>
2021-12-05 02:33:52 +0200

    Revert "Revert "add hook useSafeDispatch.js""
    This reverts commit 2fc50ff0dff6eec6f39fd816e1f04afd3b519238.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,7 +1,15 @@
+import { useSafeDispatch } from './useSafeDispatch';
 import useAsync from './useAsync';
 import useTimeout from './useTimeout';
 import useModal from './useModal';
 import useLogIn from './useLogIn';
 import useCopyToClipboard from './useCopyToClipboard';
 
-export { useAsync, useTimeout, useModal, useLogIn, useCopyToClipboard };
+export {
+  useSafeDispatch,
+  useAsync,
+  useTimeout,
+  useModal,
+  useLogIn,
+  useCopyToClipboard,
+};
