b57d648cb2c22f7cf6254f60c0452f37e4b74a99
melnicenkovadik <melnicenkovadik@gmail.com>
2021-12-08 13:50:02 +0200

    update

--- src/hooks/useLogIn.js
+++ src/hooks/useLogIn.js
@@ -5,23 +5,30 @@ import { useWeb3React } from '@web3-react/core';
 
 import InstallMetamaskAlert from '../pages/Home/components/InstallMetamaskAlert';
 import { connectorsByName, ethereum, STAKING_PAGE } from '../config';
+import { useMedia } from '../hooks/index';
 
 const useLogIn = () => {
   const history = useHistory();
   const { activate } = useWeb3React();
+  const isSmall = useMedia('(max-width: 699px)');
 
   const logIn = async () => {
     await activate(connectorsByName.Injected);
-    history.push(STAKING_PAGE);
-
-    if (!ethereum?.isMetaMask) {
-      return alertStore.addNotification({
-        content: InstallMetamaskAlert,
-        container: 'bottom-right',
-        animationIn: ['animated', 'fadeIn'],
-        animationOut: ['animated', 'fadeOut'],
-      });
+    if (isSmall) {
+      return window.location.replace(
+          'https://metamask.app.link/dapp/dev.d2nndxolfp1vk8.amplifyapp.com/',
+      );
+    } else {
+      if (!ethereum && !ethereum?.isMetaMask) {
+        return alertStore.addNotification({
+          content: InstallMetamaskAlert,
+          container: 'bottom-right',
+          animationIn: ['animated', 'fadeIn'],
+          animationOut: ['animated', 'fadeOut'],
+        });
+      }
     }
+    history.push(STAKING_PAGE);
     return false;
   };
   return { logIn };
