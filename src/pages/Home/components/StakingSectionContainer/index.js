b19deda395f7a723d1347ebc74c393d314028c3d
melnicenkovadik <melnicenkovadik@gmail.com>
2022-02-11 11:44:37 +0200

    last fixes

--- src/pages/Home/components/StakingSectionContainer/index.js
+++ src/pages/Home/components/StakingSectionContainer/index.js
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-
+import { ReactSVG } from 'react-svg';
 import {
   injected,
   MAIN_PAGE,
@@ -14,28 +14,46 @@ import { Loader } from '../../../../components/Loader';
 import Paragraph from '../../../../components/Paragraph';
 import Button from '../../../../components/Button';
 import { useLogIn, useMedia, useModal } from 'hooks';
-import { debugLog } from 'utils/helpers';
+import { debugLog, poolIcon } from 'utils/helpers';
 import Modal from 'components/Modal';
 import ButtonGroup from 'components/ButtonGroup';
+import appStore from 'store/app.store';
+
+import chevronUp from 'assets/svg/Chevron-up.svg';
+import chevronDown from 'assets/svg/Chevron-down.svg';
+import paragrapfIcon from 'assets/svg/paragrapf-icon.svg';
 
 export default () => {
   const { pathname } = useLocation();
   const [pools, getPools] = useContext(PoolsContext);
   const [openDropDown, setOpenDropDown] = useState(false);
-  const [chartData, setChartData] = useState(data[0]);
-  const [pickedName, setPickedName] = useState(poolsArr[0]);
-
+  const [pickedName, setPickedName] = useState({});
+  const [rangeValue, setRangeValue] = useState(0);
+  const [calculateValue, setCalculateValue] = useState(0);
+  const [historyCalculating, setHistoryCalculating] = useState([
+    {
+      myStake: 0,
+      myReward: 0,
+    },
+  ]);
   const isSmall = useMedia('(max-width: 699px)');
   const { logIn } = useLogIn();
   const { isShowing: isLogInMethodShow, toggle: toggleLogInMethodShow } =
     useModal();
 
+  useEffect(() => {
+    if (pools.length > 0) {
+      if (pickedName.index === undefined) {
+        pools.forEach((pool) => pool.active && setPickedName(pool));
+      }
+    }
+  }, [pools, pickedName]);
+
   const openDropDownHandler = () => {
     setOpenDropDown(!openDropDown);
   };
 
-  const chartDataHandler = (arr) => {
-    setChartData(arr);
+  const chartDataHandler = () => {
     openDropDownHandler();
   };
 
@@ -43,6 +61,21 @@ export default () => {
     setPickedName(contractName);
   };
 
+  const handleChangeRange = (e) => {
+    setRangeValue(e.target.value);
+    if (e.target.value && pickedName.poolAPY && appStore.tokenPrice) {
+      setCalculateValue(
+        +e.target.value * +pickedName.poolAPY * appStore.tokenPrice,
+      );
+      setHistoryCalculating((prevState) => [
+        ...prevState,
+        {
+          myStake: +rangeValue,
+          myReward: +calculateValue,
+        },
+      ]);
+    }
+  };
 
   const isMainPage = pathname === MAIN_PAGE;
   useEffect(() => {
@@ -160,64 +193,108 @@ export default () => {
       </div>
       <div className="staking-calculator">
         <div className="container">
-          <div
-              className="pool-picker"
-              style={{
-                height: !openDropDown ? 44 : 'auto',
-                overflow: !openDropDown ? 'hidden' : 'auto',
-              }}
-          >
-            <div
-                role="presentation"
-                onClick={openDropDownHandler}
-                className="pool-picker-arrow"
-            >
-              <ReactSVG src={openDropDown ? chevronUp : chevronDown} />
-            </div>
-            <div className="chart__pool-picker--name" role="presentation">
-              <ReactSVG
-                  className="chart__pool-picker--name--icon"
-                  src={poolIcon(pickedName.index)}
-                  wrapper="div"
-              />
-              {pickedName.contractName}
-            </div>
-            {poolsArr.map(
-                (pool, index) =>
-                    pool.contractName !== pickedName.contractName && (
-                        <div
-                            key={pool.contractName}
-                            className="chart__pool-picker--name"
-                            onClick={() => {
-                              chartDataHandler(data[index]);
-                              setPickedNameHandler(pool);
-                            }}
-                            role="presentation"
-                        >
-                          <ReactSVG
-                              className="chart__pool-picker--name--icon"
-                              src={poolIcon(pool.index)}
-                              wrapper="div"
-                          />
-                          {pool.contractName}
-                        </div>
-                    ),
-            )}
-          </div>
           <div className="info">
-            <div>Lorem ipsum dolor sit</div>
-            <div>
+            <div className="after-icon">
+              <ReactSVG src={paragrapfIcon} wrapper="span" />
+            </div>
+            <div className="info__primary">Lorem ipsum dolor sit</div>
+            <div className="info__secondary">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
               diam, cursus interdum dictum interdum. Malesuada platea vehicula
               ac, mattis et eget. Vel egestas aliquam semper amet, leo.
             </div>
           </div>
           <div className="calculator">
-            <div>Lorem ipsum dolor sit</div>
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-              diam, cursus interdum dictum interdum. Malesuada platea vehicula
-              ac, mattis et eget. Vel egestas aliquam semper amet, leo.
+            {pools.length > 0 && (
+              <div
+                className="pool-picker"
+                style={{
+                  height: !openDropDown ? 44 : 'auto',
+                  overflow: !openDropDown ? 'hidden' : 'auto',
+                }}
+              >
+                <div
+                  role="presentation"
+                  onClick={openDropDownHandler}
+                  className="arrow"
+                >
+                  <ReactSVG src={openDropDown ? chevronUp : chevronDown} />
+                </div>
+                <div className="pool-picker--name" role="presentation">
+                  <ReactSVG
+                    className="pool-picker--name--icon"
+                    src={poolIcon(pickedName.index)}
+                    wrapper="div"
+                  />
+                  {pickedName.contractName}
+                </div>
+                {pools.map(
+                  (pool) =>
+                    // pool.active &&
+                    pool.contractName !== pickedName.contractName && (
+                      <div
+                        key={pool.contractName}
+                        className="pool-picker--name"
+                        onClick={() => {
+                          chartDataHandler();
+                          setPickedNameHandler(pool);
+                        }}
+                        role="presentation"
+                      >
+                        <ReactSVG
+                          className="pool-picker--name--icon"
+                          src={poolIcon(pool.index)}
+                          wrapper="div"
+                        />
+                        {pool.contractName}
+                      </div>
+                    ),
+                )}
+              </div>
+            )}
+            <div className="calculator__labels">
+              <div className="left-value">
+                <div className="secondary">Caption</div>
+                <div className="primary">{rangeValue}</div>
+              </div>
+              <div className="right-value">
+                <div className="secondary">Caption</div>
+                <div className="primary">{pickedName.poolAPY}%</div>
+              </div>
+            </div>
+            <div className="calculator__range">
+              {/* TODO min & max range value */}
+              <input
+                min="1000"
+                max="100000"
+                value={rangeValue}
+                step="1"
+                onChange={handleChangeRange}
+                type="range"
+              />
+            </div>
+            <div className="calculator__results">
+              {historyCalculating
+                .reverse()
+                .filter((item, index) => index < 3)
+                .map((historyItem, index) => (
+                  // eslint-disable-next-line
+                  <div key={index} className="calculator__results__result">
+                    <div>Caption</div>
+                    <div>{historyItem.myStake} AMB</div>
+                    <div>${historyItem.myReward.toFixed(2)}</div>
+                    <div className="hr" />
+                  </div>
+                ))}
+              <Button
+                buttonStyles={{ width: 174, height: 65, marginLeft: 'auto' }}
+                type="white"
+                onclick={() => {
+                  toggleLogInMethodShow();
+                }}
+              >
+                ↖ Stake Now
+              </Button>
             </div>
           </div>
         </div>
