c12ec3b592390a83982ab66ae2011c4478ed2ce1
melnicenkovadik <melnicenkovadik@gmail.com>
2022-02-15 15:36:04 +0200

    fixes

--- src/pages/Home/components/StakingSectionContainer/index.js
+++ src/pages/Home/components/StakingSectionContainer/index.js
@@ -14,7 +14,7 @@ import { Loader } from '../../../../components/Loader';
 import Paragraph from '../../../../components/Paragraph';
 import Button from '../../../../components/Button';
 import { useLogIn, useMedia, useModal } from 'hooks';
-import { debugLog } from 'utils/helpers';
+import { debugLog, poolIcon } from 'utils/helpers';
 import Modal from 'components/Modal';
 import ButtonGroup from 'components/ButtonGroup';
 import appStore from 'store/app.store';
@@ -23,8 +23,9 @@ import paragrapfIcon from 'assets/svg/paragrapf-icon.svg';
 export default () => {
   const { pathname } = useLocation();
   const [pools, getPools] = useContext(PoolsContext);
-  const [rangeValue, setRangeValue] = useState(1000);
+  const [rangeValue, setRangeValue] = useState(50000);
   const inputRef = useRef(null);
+  const isMainPage = pathname === MAIN_PAGE;
 
   const isSmall = useMedia('(max-width: 699px)');
   const { logIn } = useLogIn();
@@ -34,16 +35,17 @@ export default () => {
   const handleChangeRange = (e) => {
     if (e.target.value && appStore.tokenPrice && inputRef.current !== null) {
       setRangeValue(e.target.value);
-      const value = ((e.target.value - 1000) / (100000 - 1000)) * 100;
-      inputRef.current.style.background = `linear-gradient(to right, rgba(21, 211, 120,0.9) 0%, rgba(54, 15, 140, 0.8) ${value}%, transparent ${value}%, transparent 100%)`;
     }
   };
 
-  const isMainPage = pathname === MAIN_PAGE;
   useEffect(() => {
+    if (inputRef.current !== null) {
+      const value = (rangeValue / (100000 - 1000)) * 100;
+      inputRef.current.style.background = `linear-gradient(to right, rgba(21, 211, 120,0.9) 0%, rgba(54, 15, 140, 0.8) ${value}%, transparent ${value}%, transparent 100%)`;
+    }
     debugLog('Home render useEffect');
     getPools();
-  }, []);
+  }, [rangeValue]);
 
   return (
     <>
@@ -204,7 +206,19 @@ export default () => {
                       key={pool.contractName}
                       className="calculator__results__result"
                     >
-                      <div style={{ justifyContent: 'flex-start' }}>
+                      <div
+                        style={{
+                          justifyContent: 'flex-start',
+                          alignItems: 'center',
+                          gap: 10,
+                          justifyItems: 'flex-start',
+                        }}
+                      >
+                        <ReactSVG
+                          className="pool-calc-icon"
+                          src={poolIcon(pool.index)}
+                          wrapper="span"
+                        />
                         {pool.contractName}
                       </div>
                       <div style={{ justifyContent: 'flex-start' }}>
