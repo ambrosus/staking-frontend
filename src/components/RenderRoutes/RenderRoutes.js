5918783794e4e9d56d80c8b0dcc27e4de1cfbc41
dimavoronin <zveruha1109>
2022-10-24 20:11:32 +0300

    reset to app

--- src/components/RenderRoutes/RenderRoutes.js
+++ src/components/RenderRoutes/RenderRoutes.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import { Route, Switch } from 'react-router';
-import { observer } from 'mobx-react-lite';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router";
+import { observer } from "mobx-react-lite";
+import { BrowserRouter } from "react-router-dom";
 
-// import Home from '../../pages/Home';
-import HomeStatic from '../../pages/HomeStatic';
-// import Staking from '../../pages/Staking';
-import { MAIN_PAGE } from 'config';
-import PoolsContextProvider from '../../context/poolsContext';
-// import Footer from 'components/layouts/Footer';
+import Home from "../../pages/Home";
+import Staking from "../../pages/Staking";
+import { MAIN_PAGE, STAKING_PAGE } from "config";
+import PoolsContextProvider from "../../context/poolsContext";
+import Footer from "components/layouts/Footer";
 // eslint-disable-next-line
 
 const RenderRoutes = observer(() => (
   <PoolsContextProvider>
     <BrowserRouter>
       <Switch>
-        <Route path={MAIN_PAGE} exact render={() => <HomeStatic />} />
-        {/* <Route path={STAKING_PAGE} exact render={() => <Staking />} /> */}
+        <Route path={MAIN_PAGE} exact render={() => <Home />} />
+        <Route path={STAKING_PAGE} exact render={() => <Staking />} />
       </Switch>
-      <React.Suspense fallback={<div />}>{/* <Footer /> */}</React.Suspense>
+      <React.Suspense fallback={<div />}>
+        <Footer />
+      </React.Suspense>
     </BrowserRouter>
   </PoolsContextProvider>
 ));
